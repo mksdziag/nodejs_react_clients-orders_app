264d93883822656b58ceb72ddfff7723bc4b62c6
Maks <mksdziag@gmail.com>
2019-01-30 08:35:01 +0100

    clean unnecesary

--- app_front/src/components/Home.js
+++ app_front/src/components/Home.js
@@ -9,65 +9,65 @@ import addClientIcon from '../assets/iconmonstr-user-add.svg';
 import aggrIcon from '../assets/iconmonstr-cube.svg';
 
 const Home = () => {
-  return (
-    <Fragment>
-      <SiteHeader title="Appstract Recruitment App" />
-      <div className="tile is-ancestor home-tiles">
-        <div className="tile is-vertical is-8">
-          <div className="tile">
-            <div className="tile is-parent is-vertical">
-              <LinkTile
-                classes="is-warning"
-                bgIcon={ordersIcon}
-                linkPath="/orders"
-                title="Orders"
-                subtitle="Manage all orders"
-              />
-            </div>
-            <div className="tile is-parent">
-              <LinkTile
-                classes="is-warning"
-                bgIcon={addOrderIcon}
-                linkPath="/orders/add-new"
-                title="Add New Order"
-                subtitle="Manage new orders"
-              />
-            </div>
-          </div>
-          <div className="tile">
-            <div className="tile is-parent is-vertical">
-              <LinkTile
-                classes="is-dark"
-                bgIcon={clientsIcon}
-                linkPath="/clients"
-                title="Clients"
-                subtitle="Manage all clients"
-              />
-            </div>
-            <div className="tile is-parent">
-              <LinkTile
-                classes="is-dark"
-                bgIcon={addClientIcon}
-                linkPath="/clients/add-new"
-                title="Add New Client"
-                subtitle="Manage new clients"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="tile is-parent">
-          <LinkTile
-            classes="is-primary"
-            bgIcon={aggrIcon}
-            linkPath="/clients-orders"
-            title="Grouped orders"
-            subtitle="Aggregate orders data"
-            backgroundSize="auto 30%"
-          />
-        </div>
-      </div>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<SiteHeader title="Recruitment App" />
+			<div className="tile is-ancestor home-tiles">
+				<div className="tile is-vertical is-8">
+					<div className="tile">
+						<div className="tile is-parent is-vertical">
+							<LinkTile
+								classes="is-warning"
+								bgIcon={ordersIcon}
+								linkPath="/orders"
+								title="Orders"
+								subtitle="Manage all orders"
+							/>
+						</div>
+						<div className="tile is-parent">
+							<LinkTile
+								classes="is-warning"
+								bgIcon={addOrderIcon}
+								linkPath="/orders/add-new"
+								title="Add New Order"
+								subtitle="Manage new orders"
+							/>
+						</div>
+					</div>
+					<div className="tile">
+						<div className="tile is-parent is-vertical">
+							<LinkTile
+								classes="is-dark"
+								bgIcon={clientsIcon}
+								linkPath="/clients"
+								title="Clients"
+								subtitle="Manage all clients"
+							/>
+						</div>
+						<div className="tile is-parent">
+							<LinkTile
+								classes="is-dark"
+								bgIcon={addClientIcon}
+								linkPath="/clients/add-new"
+								title="Add New Client"
+								subtitle="Manage new clients"
+							/>
+						</div>
+					</div>
+				</div>
+				<div className="tile is-parent">
+					<LinkTile
+						classes="is-primary"
+						bgIcon={aggrIcon}
+						linkPath="/clients-orders"
+						title="Grouped orders"
+						subtitle="Aggregate orders data"
+						backgroundSize="auto 30%"
+					/>
+				</div>
+			</div>
+		</Fragment>
+	);
 };
 
 export default Home;
--- app_front/src/components/NavBar.js
+++ app_front/src/components/NavBar.js
@@ -1,52 +1,51 @@
 import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import logo from '../assets/logo-light.png';
 
 class NavBar extends Component {
-  state = { isExpanded: false };
+	state = { isExpanded: false };
 
-  toggleMobileNav = () => {
-    this.setState(prevState => ({ isExpanded: !prevState.isExpanded }));
-  };
+	toggleMobileNav = () => {
+		this.setState(prevState => ({ isExpanded: !prevState.isExpanded }));
+	};
 
-  render() {
-    const { isExpanded } = this.state;
-    return (
-      <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <Link to="" className="navbar-item">
-            <img src={logo} alt="app logo" className="navbar-logo" />
-          </Link>
+	render() {
+		const { isExpanded } = this.state;
+		return (
+			<nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+				<div className="navbar-brand">
+					<Link to="" className="navbar-item">
+						AppLogo
+					</Link>
 
-          <span
-            onClick={this.toggleMobileNav}
-            role="button"
-            className={`navbar-burger burger ${isExpanded && 'is-active'}`}
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-          </span>
-        </div>
+					<span
+						onClick={this.toggleMobileNav}
+						role="button"
+						className={`navbar-burger burger ${isExpanded && 'is-active'}`}
+						aria-label="menu"
+						aria-expanded="false"
+						data-target="navbarBasicExample"
+					>
+						<span aria-hidden="true" />
+						<span aria-hidden="true" />
+						<span aria-hidden="true" />
+					</span>
+				</div>
 
-        <div id="navbarBasicExample" className={`navbar-menu ${isExpanded && 'is-active'}`}>
-          <div className="navbar-start" />
+				<div id="navbarBasicExample" className={`navbar-menu ${isExpanded && 'is-active'}`}>
+					<div className="navbar-start" />
 
-          <div className="navbar-end">
-            <NavLink to="/orders" className="navbar-item" activeClassName="is-active">
-              Orders
-            </NavLink>
-            <NavLink to="/clients" className="navbar-item" activeClassName="is-active">
-              Clients
-            </NavLink>
-          </div>
-        </div>
-      </nav>
-    );
-  }
+					<div className="navbar-end">
+						<NavLink to="/orders" className="navbar-item" activeClassName="is-active">
+							Orders
+						</NavLink>
+						<NavLink to="/clients" className="navbar-item" activeClassName="is-active">
+							Clients
+						</NavLink>
+					</div>
+				</div>
+			</nav>
+		);
+	}
 }
 
 export default NavBar;
